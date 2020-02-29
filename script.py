3a92ddb411baa97dcd4fe34f92e05f2149fbc6b0
mkkaufmann <mkaufmann421@gmail.com>
2020-02-28 22:57:16 -0500

    Comment code

--- script.py
+++ script.py
@@ -1,22 +1,34 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# change this file name to the spreadsheet
 f = open("file.csv", "r")
 
-comp = "stjoe"
+# (optional) tag matches with a competition
+comp = ""
+# weights for overall score (tentative numbers)
 weights = [0, 0, 0, 2, 4, 6, 0, 1, 2, 3, 10, 20, 1, 2, 3, 0, 0, 0, -0.25]
+# dictionary for keeping track of the teams
 teams = {}
 if f.mode == 'r':
     contents = f.readlines()
+    # read all of the labels
     labels = contents[0].replace('"','').split(",")
+    # loop through scouting data
     for i in range(1, len(contents)):
         data = contents[i].split(',')
+        # team number is first value
         team = int(data[0].replace('"',''))
+        # match number is second value
         match = int(data[1].replace('"',''))
         matchtag = comp + " match " + str(match)
+        # initialize uninitialized team dictionaries
         if not team in teams:
             teams[team] = {}
+        # add match to dictionary
         teams[team][matchtag] = {}
+
+        # initialize sum counters
         ballScore = 0
         climbScore = 0
         controlScore = 0
@@ -24,6 +36,7 @@ if f.mode == 'r':
         score = 0
         for j in range(3, len(data)):
             value = data[j].replace('"','')
+            #rotational control
             if j == 10:
                 if value == "y":
                     score+=10
@@ -32,6 +45,7 @@ if f.mode == 'r':
                 else:
                     teams[team][matchtag][labels[j]]=0
                 continue
+            #position control
             elif j == 11:
                 if value == "y":
                     score += 20
@@ -40,9 +54,11 @@ if f.mode == 'r':
                 else:
                     teams[team][matchtag][labels[j]]=0
                 continue
+            #scoring locations
             elif j == 15:
                 teams[team][matchtag][labels[j]]=value
                 continue
+            #hang
             elif j == 16:
                 if value == "L":
                     teams[team][matchtag][labels[j]]=40
@@ -59,6 +75,7 @@ if f.mode == 'r':
                 else:
                     teams[team][matchtag][labels[j]]=0
                 continue
+            #defense and climb
             elif j == 17 or j==18:
                 teams[team][matchtag][labels[j]]=int(value)
             else:
@@ -67,12 +84,14 @@ if f.mode == 'r':
                 score+=int(value)*weights[j]
                 ballScore+=int(value)*weights[j]
                 totalBalls+=1
+        #populate some general statistics from the match
         teams[team][matchtag]["Overall Score"] = score
         teams[team][matchtag]["Ball Score"] = ballScore
         teams[team][matchtag]["Climb Score"] = climbScore
         teams[team][matchtag]["Total Balls"] = totalBalls
         teams[team][matchtag]["Control Score"] = controlScore
 
+#used to get the average value of a metric for a team
 def getAverageBetweenMatches(teams, team, datapoint):
     total = 0
     amount = 0
@@ -85,9 +104,11 @@ metrics = ["Bottom Port Auto", "Outer Port Auto", "Inner Port Auto", "PC Pickup 
 print("data on a specific team? y/n")
 specific = input()
 if specific == "y":
+    # get all data for a single team
     print("team?")
     team = int(input())
     labels = ["Bottom Port Auto","Outer Port Auto","Inner Port Auto","PC Pickup Auto","Bottom Port Tele","Outer Port Tele","Inner Port Tele","Rotation Control","Position Control","Bottom Port Endgame","Outer Port Endgame","Inner Port Endgame","Endgame Outcome","Defense Second", "Overall Score", "Ball Score", "Climb Score", "Total Balls"]
+    # graph
     ind = np.arange(len(labels))
     values = []
     for label in labels:
@@ -99,8 +120,10 @@ if specific == "y":
     plt.title(team)
     plt.show()
 else:
+    #get single metric for all teams
     labels = list(teams.keys())
-
+    
+    # print metric options
     print("0 | Bottom Port Auto")
     print("1 | Outer Port Auto")
     print("2 | Inner Port Auto")
@@ -119,6 +142,8 @@ else:
     print("15| Total Balls")
     print("16| Control Score")
     print("enter the desired data point")
+
+    #graph
     datapoint = metrics[int(input())]
     ind = np.arange(len(labels))
     values = []
@@ -130,5 +155,3 @@ else:
     plt.xticks(ind, labels, fontsize=7, rotation=30)
     plt.title(datapoint)
     plt.show()
-
-
