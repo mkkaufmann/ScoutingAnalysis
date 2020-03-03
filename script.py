184d157be46dc01a88d0f6c2e9190be8365a43a0
mkkaufmann <mkaufmann421@gmail.com>
2020-03-03 17:39:54 -0500

    Consolidate Endgame and Teleop Balls

--- script.py
+++ script.py
@@ -33,6 +33,13 @@ if f.mode == 'r':
         climbScore = 0
         controlScore = 0
         totalBalls = 0
+        autoBalls = 0
+        teleopBalls = 0
+        teleopBallsBottom = 0
+        teleopBallsOuter = 0
+        teleopBallsInner = 0
+        teleopBallScore = 0
+        autoBallScore = 0
         score = 0
         for j in range(3, len(data)):
             value = data[j].replace('"','')
@@ -45,7 +52,37 @@ if f.mode == 'r':
                 else:
                     teams[team][matchtag][labels[j]]=0
                 continue
-            #position control
+            elif j>=3 and j<=5:
+                balls = int(value)
+                autoBalls+=balls
+                score+=balls*weights[j]
+                autoBallScore+=balls*weights[j]
+                ballScore+=balls*weights[j]
+                teams[team][matchtag][labels[j]]=int(value)
+            elif j==7 or j==12:
+                balls = int(value)
+                teleopBalls+=balls
+                teleopBallsBottom+=balls
+                score+=balls*weights[j]
+                teleopBallScore+=balls*weights[j]
+                ballScore+=balls*weights[j]
+                teams[team][matchtag][labels[j]]=int(value)
+            elif j==8 or j==13:
+                balls = int(value)
+                teleopBalls+=balls
+                teleopBallsOuter+=balls
+                score+=balls*weights[j]
+                teleopBallScore+=balls*weights[j]
+                ballScore+=balls*weights[j]
+                teams[team][matchtag][labels[j]]=int(value)
+            elif j==9 or j==14:
+                balls = int(value)
+                teleopBalls+=balls
+                teleopBallsInner+=balls
+                score+=balls*weights[j]
+                teleopBallScore+=balls*weights[j]
+                ballScore+=balls*weights[j]
+                teams[team][matchtag][labels[j]]=int(value)
             elif j == 11:
                 if value == "y":
                     score += 20
@@ -80,13 +117,17 @@ if f.mode == 'r':
                 teams[team][matchtag][labels[j]]=int(value)
             else:
                 teams[team][matchtag][labels[j]]=int(value)
-                increase = int(value)*weights[j]
                 score+=int(value)*weights[j]
                 ballScore+=int(value)*weights[j]
                 totalBalls+=int(value)
         #populate some general statistics from the match
         teams[team][matchtag]["Overall Score"] = score
-        teams[team][matchtag]["Ball Score"] = ballScore
+        teams[team][matchtag]["Total Ball Score"] = ballScore
+        teams[team][matchtag]["Auto Ball Score"] = autoBallScore
+        teams[team][matchtag]["Teleop Ball Score"] = teleopBallScore
+        teams[team][matchtag]["Teleop Balls Bottom"] = teleopBallsBottom
+        teams[team][matchtag]["Teleop Balls Outer"] = teleopBallsOuter
+        teams[team][matchtag]["Teleop Balls Inner"] = teleopBallsInner
         teams[team][matchtag]["Climb Score"] = climbScore
         teams[team][matchtag]["Total Balls"] = totalBalls
         teams[team][matchtag]["Control Score"] = controlScore
@@ -100,7 +141,7 @@ def getAverageBetweenMatches(teams, team, datapoint):
         amount+=1
     return total/amount
 
-metrics = ["Bottom Port Auto", "Outer Port Auto", "Inner Port Auto", "PC Pickup Auto", "Bottom Port Tele", "Outer Port Tele", "Inner Port Tele", "Rotation Control", "Position Control", "Bottom Port Endgame", "Outer Port Endgame", "Inner Port Endgame", "Overall Score", "Ball Score", "Climb Score", "Total Balls", "Control Score"]
+metrics = ["Bottom Port Auto", "Outer Port Auto", "Inner Port Auto", "PC Pickup Auto", "Teleop Balls Bottom", "Teleop Balls Outer", "Teleop Balls Inner", "Rotation Control", "Position Control", "Overall Score", "Total Ball Score", "Climb Score", "Total Balls", "Control Score", "Auto Ball Score", "Teleop Ball Score"]
 print("1 | data on a specific team")
 print("2 | single metric across all teams")
 print("3 | compare multiple teams")
@@ -143,14 +184,13 @@ elif graphType == "2":
     print("6 | Inner Port Tele")
     print("7 | Rotation Control")
     print("8 | Position Control")
-    print("9 | Bottom Port Endgame")
-    print("10| Outer Port Endgame")
-    print("11| Inner Port Endgame")
-    print("12| Overall Score")
-    print("13| Ball Score")
-    print("14| Climb Score")
-    print("15| Total Balls")
-    print("16| Control Score")
+    print("9 | Overall Score")
+    print("10| Total Ball Score")
+    print("11| Climb Score")
+    print("12| Total Balls")
+    print("13| Control Score")
+    print("14| Auto Ball Score")
+    print("15| Teleop Ball Score")
     print("enter the desired data point")
 
     #graph
@@ -159,7 +199,7 @@ elif graphType == "2":
     values = []
     for team in labels:
         values.append(getAverageBetweenMatches(teams, team, datapoint))
-    sortedindices = np.argsort(values)
+    sortedindices = np.flip(np.argsort(values))
     sortedlabels=[]
     sortedvalues=[]
     for i in range(len(sortedindices)):
@@ -184,12 +224,12 @@ elif graphType == "3":
     labels = ["Bottom Port Auto","Outer Port Auto","Inner Port Auto","PC Pickup Auto","Bottom Port Tele","Outer Port Tele","Inner Port Tele","Rotation Control","Position Control","Bottom Port Endgame","Outer Port Endgame","Inner Port Endgame","Endgame Outcome","Defense Second", "Overall Score", "Ball Score", "Climb Score", "Total Balls"]
     # graph
     ind = np.arange(len(labels))
-    width = 0.85/len(selected)
+    width = 0.75/len(selected)
     for i in range(len(selected)):
         values = []
         for label in labels:
             values.append(getAverageBetweenMatches(teams, selected[i], label))
-        plt.bar(ind + (i-0.5)*width, values, width, label=selected[i])
+        plt.bar(ind + (i-len(selected)/2.0 + 0.5)*width, values, width, label=selected[i])
     plt.xlabel('Category', fontsize=7)
     plt.ylabel('Value', fontsize=7)
     plt.xticks(ind, labels, fontsize=7, rotation=30)
